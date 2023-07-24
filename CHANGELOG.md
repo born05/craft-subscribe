d40e25b0ee1f98a044796a3d43bbed4c9248cec1
Roel van Hintum <roel@born05.com>
2023-07-24 13:32:14 +0200

    ump version for craft 4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,14 @@
 Changelog
 ==================
 
+## 3.0.0 - 2023-07-24
+### Fixes
+- Bump version for craft 4
+
+## 2.2.1 - 2023-07-24
+### Fixes
+- Improve craft 3 compatibility
+
 ## 2.2.0 - 2023-03-06
 ### Changes
 - Initial public release
--- src/controllers/SubscribeController.php
+++ src/controllers/SubscribeController.php
@@ -16,7 +16,7 @@ class SubscribeController extends Controller
     /**
      * @inheritdoc
      */
-    protected array|int|bool $allowAnonymous = true;
+    protected array|int|bool $allowAnonymous = self::ALLOW_ANONYMOUS_LIVE;
 
     // Public Methods
     // =========================================================================
